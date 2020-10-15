925c7db369b0ae31e4ff826e228c525e2d3d5b31
darshi99 <70588966+darshi99@users.noreply.github.com>
2020-10-15 09:29:59 -0400

    Add files via upload

--- accept_reject.m
+++ accept_reject.m
@@ -1,9 +1,18 @@
 function
 if
-    (rand < exp(-beta*deltaE))=1
+    %initial energry, change in energy and beta
+    
+    (rand < exp(-beta*deltaE)) = 1
                 % Accept displacement move
-    (rand > exp(-beta*deltaE)) =-1
-    % Reject displacement move
-                coords(:,i) = rTrial;       % Update positions
+    (rand > exp(-beta*deltaE)) = -1  
+    % reject displacement move
+    
+    (-deltaE > 0) = 2
+    % accept when old energy coordinates is less than new energy
+    
+     (-deltaE < 0) = -2  
+     % reject when old energy coordinates are more than new energy
+     
+     coords(:,i) = rTrial;       % Update positions
                 energy = energy + deltaE;   % Update energy
-            end
+end
