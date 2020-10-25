db964dd140cd0d2890d2d3df0d5115d791ad2c81
shedaya1 <55414913+shedaya1@users.noreply.github.com>
2020-10-25 09:08:08 -0400

    Add files via upload

--- sarvenaz/min_distance_condition.m
+++ sarvenaz/min_distance_condition.m
@@ -1,4 +1,4 @@
-function coordinates = min_distance_condition(coords,L)
+function condition = min_distance_condition(coords,L)
 %% This function currently not in use - doesn't run
 % This function takes in the coordinates and determines if any distance is
 % less than the minimum spacing of rc = 1. If so, the function returns 0
@@ -6,9 +6,7 @@ function coordinates = min_distance_condition(coords,L)
 % If it does meet the condition, returns 1.
 
 N = size(coords,2);
-%condition = 1;
-coordinates=zeros(3,1);
-coordinates(:,(1:N))=coords(:,(1:N));
+condition = 1;
 for partA = N
     for partB = 1:N
         if partB == partA
@@ -16,36 +14,21 @@ for partA = N
         else
             % compute distance
             r = coords(:,partA) - coords(:,partB);
-            
-            
-            
-            for component = 1:3   % minimum image criterion
-                if abs(r(component,1)) < 1
-                    if coords(component,partA)>0
-                        coordinates(component,partA) = 1 + coordinates(component,partA);
-                    end
-                    if coords(component,partA)<0
-                        coordinates(component,partA) = -1 + coordinates(component,partA);
-                    end
-                else
-                    continue
-                    
-                end
-            end           
 
             % distance squared
-%            r_2 = sum(dot(r,r));
+            r_2 = sum(dot(r,r));
 
- %           if r_2 < 1
- %               condition = 0;
-  %              break
-   %         end
-    %    end
-    %    if condition == 0
-     %       break
-      %  end
-%    if condition == 0
- %       break
+            if r_2 < 1
+                condition = 0;
+                break
+            end
         end
+        if condition == 0
+            break
+        end
+    if condition == 0
+        break
+    end
     end
 end
+end 
